0db76c516dbf622571f8c2983121d70af15cbfad
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-20 13:58:55 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div></div>
+      <main className="flex min-h-screen w-full max-w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div>
+          app
+        </div>
       </main>
     </div>
   );
