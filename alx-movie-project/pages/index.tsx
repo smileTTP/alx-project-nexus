47b5b1eb0e7d591dcb9862b2babb6eeba120a942
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-22 20:41:01 +0100

    .

--- alx-movie-project/pages/index.tsx
+++ alx-movie-project/pages/index.tsx
@@ -16,6 +16,12 @@ export default function Home() {
         <div className="bg-white w-full h-[274px] flex items-center justify-center">
           <p className="text-[#591427] font-bold text-3xl md:text-[45px] text-center mt-4">“Oh how Shakespeare would have loved cinema!”</p>
         </div>
+        <div>
+            <p className={`${blackhansans.className} text-white underline text-3xl px-4 mt-4`}>Trending Today</p>
+            <div className="flex justify-center items-center mb-8 mt-4">
+            <button className="bg-[#8080FF] w-[174px] h-[50px] rounded-[10px] text-white text-[20px]">Show more</button>
+            </div>
+        </div>
       </main>
     </div>
   );
