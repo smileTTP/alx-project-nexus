00bf6eccd5252818e7b2c8d6a8223624af1d2d71
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-22 19:33:57 +0100

    header

--- alx-movie-project/components/Layout/header.tsx
+++ alx-movie-project/components/Layout/header.tsx
@@ -1,9 +1,71 @@
 import Link from "next/link";
 import Image from "next/image";
+import { CiSearch } from "react-icons/ci";
+import { IoIosMenu, IoIosClose } from "react-icons/io";
+import { useState } from "react";
+
 const Header: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+    const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    if (isSearchOpen) setIsSearchOpen(false);
+    };
+    const toggleSearch = () => {
+        setIsSearchOpen(!isSearchOpen);
+        if (isMenuOpen) setIsMenuOpen(false);
+    }
+
     return (
-        <header>
-            <div><Image src={'/assets/LOGO.svg'} alt="" height={60} width={302}/></div>
+        <header className="bg-[#96D9C0] p-4 sticky w-full">
+            <nav className="flex justify-between items-center p-4 mx-auto">
+            <div className="px-4"><Image src={'/assets/LOGO.svg'} alt="PELLICLA" height={50} width={200} className="w-[150px] md:w-[200px]"/></div>
+            <div className="hidden md:flex space-x-10 items-center">
+                <Link href="/" className="hover:text-[#591427]">
+                <p className="text-[#4C3A51] text-[30px]">Home</p>
+                </Link>
+                <p className="text-[#4C3A51] text-[30px]">Movies</p>
+                <p className="text-[#4C3A51] text-[30px]">Dashboard</p>
+            </div>
+            <div className="flex items-center space-x-10 px-4">
+                <button onClick={toggleSearch} className="text-[#4C3A51] focus:outline-none">
+                {isSearchOpen ? (
+                <IoIosClose className="text-[#4C3A51] text-5xl" />
+                ) : (
+                <CiSearch className="text-[#4C3A51] text-4xl" />
+                )}
+                </button>
+                <button onClick={toggleMenu} className="md:hidden text-[#4C3A51] focus:outline-none">
+                {isMenuOpen ? (
+                <IoIosClose className="text-[#4C3A51] text-5xl" />
+                ) : (
+                <IoIosMenu className="text-[#4C3A51] text-4xl" />
+                )}
+                </button>
+            </div>
+            </nav>
+            {isMenuOpen && (
+            <div className="md:hidden bg-[#4C3A51] px-4 pb-4 pt-2 space-y-4 border-t border-[#4C3A51]/10 rounded-[10px]">
+            <Link href="/">
+                <p className="block text-[#D27C91] text-[24px] py-2 border-b border-[#96D9C0]/10 hover:text-[#8080FF] rounded px-2 cursor-pointer">
+                Home</p>
+            </Link>
+            <p className="block text-[#D27C91] text-[24px] py-2 border-b border-[#96D9C0]/10 hover:text-[#8080FF] rounded px-2 cursor-pointer">
+            Movies</p>
+            <p className="block text-[#D27C91] text-[24px] py-2 border-[#96D9C0]/10 hover:text-[#8080FF] rounded px-2 cursor-pointer">
+            Dashboard</p>
+            </div>
+            )}
+
+            {isSearchOpen && (
+        <div className="top-full left-0 w-full p-4 flex justify-center items-center">
+            <div className="relative w-full max-w-3xl">
+            <input type="text" placeholder="Search for a movie" className="w-full p-3 pl-4 rounded-full border-2 border-[#4C3A51] bg-[#f1d7de] text-[#4C3A51] text-xl" />
+            <CiSearch className="absolute right-4 top-1/2 -translate-y-1/2 text-[#4C3A51] text-3xl" />
+            </div>
+        </div>
+        )}
         </header>
     )
 }
--- alx-movie-project/pages/index.tsx
+++ alx-movie-project/pages/index.tsx
@@ -8,13 +8,13 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center">
       <main className="flex w-full max-w-full bg-[#591427] flex-col">
-        <div className="bg-[#96D9C0] w-full h-[584px] flex items-center justify-center">
-        <p className={`${blackhansans.className} text-6xl md:text-[150px] text-[#4C3A51] text-center`}>
+        <div className="bg-[#96D9C0] w-full h-[560px] flex items-center justify-center">
+        <p className={`${blackhansans.className} text-[95px] md:text-[150px] text-[#4C3A51] text-center `}>
           PICTURA MOVENS
         </p>
         </div>
         <div className="bg-white w-full h-[274px] flex items-center justify-center">
-          <p className="text-[#591427] font-bold text-[45px] text-center mt-4">“Oh how Shakespeare would have loved cinema!”</p>
+          <p className="text-[#591427] font-bold text-3xl md:text-[45px] text-center mt-4">“Oh how Shakespeare would have loved cinema!”</p>
         </div>
       </main>
     </div>
