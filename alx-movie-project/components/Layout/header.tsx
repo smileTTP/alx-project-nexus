4e65ce6c2488d7adc3e36268f699699c3a140bed
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-22 17:39:45 +0100

    .

--- alx-movie-project/components/Layout/header.tsx
+++ alx-movie-project/components/Layout/header.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
-
+import Image from "next/image";
 const Header: React.FC = () => {
     return (
         <header>
-            
+            <div><Image src={'/assets/LOGO.svg'} alt="" height={60} width={302}/></div>
         </header>
     )
 }
