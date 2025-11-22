51fe72acebe69999f602ce95ce827ccd6c9788dc
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-22 18:17:14 +0100

    fonts

--- alx-movie-project/pages/index.tsx
+++ alx-movie-project/pages/index.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
+import { Black_Han_Sans } from 'next/font/google';
 
+const blackhansans = Black_Han_Sans({
+      weight: ['400']
+    });
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <main className="flex min-h-screen w-full max-w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div>
-          app
+    <div className="flex items-center justify-center">
+      <main className="flex w-full max-w-full bg-[#591427] flex-col">
+        <div className="bg-[#96D9C0] w-full h-[584px] flex items-center justify-center">
+        <p className={`${blackhansans.className} text-6xl md:text-[150px] text-[#4C3A51] text-center`}>
+          PICTURA MOVENS
+        </p>
+        </div>
+        <div className="bg-white w-full h-[274px] flex items-center justify-center">
+          <p className="text-[#591427] font-bold text-[45px] text-center mt-4">“Oh how Shakespeare would have loved cinema!”</p>
         </div>
       </main>
     </div>
