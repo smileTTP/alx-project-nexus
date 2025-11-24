16e273585928fff6258d77db388f18c6afcacbb8
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-24 22:56:09 +0100

    props

--- alx-movie-project/interfaces/index.ts
+++ alx-movie-project/interfaces/index.ts
@@ -6,6 +6,24 @@ export interface ComponentProps {
 
 export interface MovieProps {
     id: number;
-    genre_ids: number[];
+    title: string;
+    original_title: string;
+    overview: string;
+    poster_path: string;    
+    backdrop_path: string; 
+    genre_ids: number[]; 
+    release_date: string;
+    vote_average: number;
+    vote_count: number;
+    popularity: number;
+    adult: boolean;
+    video: boolean;
+    original_language: string;
+}
 
+export interface TMDBResponse {
+    page: number;
+    results: MovieProps[];
+    total_pages: number; 
+    total_results: number;
 }
