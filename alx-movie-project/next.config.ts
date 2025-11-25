0f3557d1bbbc37d6a21ae4393308236fb575bafd
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-25 19:02:50 +0100

    .

--- alx-movie-project/next.config.ts
+++ alx-movie-project/next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
