6a97a3654cc1f07b8795a13bf767412fc4de1525
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-29 17:59:45 +0100

    added login

---
+++ alx-movie-project/hooks/useTmdbAuth.ts
@@ -0,0 +1,43 @@
+import { API_KEY } from "@/constants";
+import { useCallback, useEffect, useState } from "react";
+
+const REDIRECT_PAGE = "http://localhost:3000/";
+const useTmdbAuth = () => {
+    const [newToken, setNewToken] = useState<string | null>(null);
+    const [redirectUrl, setRedirectUrl] = useState<string | null>(null);
+
+    const fetchNewToken = useCallback(async () => {
+        try {
+            const response = await fetch(`https://api.themoviedb.org/3/authentication/token/new?api_key=${API_KEY}`);
+            
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            
+            const data = await response.json();
+
+            if (data.success && data.request_token) {
+                const requestToken = data.request_token;
+                setNewToken(requestToken);
+                const authUrl = `https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=${REDIRECT_PAGE}`;
+                setRedirectUrl(authUrl);
+                
+            } else {
+                console.error("Error: TMDb did not return a valid request token.", data);
+            }
+
+        } catch (error) {
+            console.error("Error fetching new token:", error);
+        }
+    }, [API_KEY]);
+
+    useEffect(() => {
+        if (!newToken) {
+            fetchNewToken();
+        }
+    }, [fetchNewToken, newToken]);
+
+    return { newToken, redirectUrl, fetchNewToken };
+};
+
+export default useTmdbAuth;
--- alx-movie-project/pages/user/login.tsx
+++ alx-movie-project/pages/user/login.tsx
@@ -1,7 +1,36 @@
+import { CiLogin } from "react-icons/ci";
+import { SiThemoviedatabase } from "react-icons/si";
+import useTmdbAuth from "@/hooks/useTmdbAuth";
 
 const Login: React.FC = () => {
+    const { redirectUrl, fetchNewToken } = useTmdbAuth(); 
+
+    const redirectUser = async () => {
+        if (redirectUrl) {
+            window.location.href = redirectUrl;
+        } else {
+            await fetchNewToken();
+            console.log("Waiting for token to load...");
+        }
+    };
+    
+    const isDisabled = !redirectUrl; 
+
     return (
-        <div></div>
+        <div className="flex items-center justify-center min-h-screen w-full p-4">
+        <div className="p-8 bg-[#f1d7de] rounded-xl shadow-2xl border-4 border-[#4C3A51] max-w-md mx-auto">
+            <h2 className="text-3xl font-bold text-[#4C3A51] mb-4 flex items-center gap-3">
+                <CiLogin className="text-3xl"/> Log in to TMDB
+            </h2>
+            <p className="text-[#591427] mb-6">
+                To access features like favorites list, you need to link your TMDB account.
+            </p>
+            <button onClick={redirectUser} className={`w-full bg-[#4C3A51] text-[#f1d7de] font-extrabold py-3 px-6 rounded-xl transition-all shadow-md flex items-center justify-center gap-2 ${isDisabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-[#8080FF] hover:shadow-lg hover:text-[#4C3A51]'}`} disabled={isDisabled} >
+                <SiThemoviedatabase className="text-3xl"/>
+                {isDisabled ? 'Loading...' : 'Connect with TMDb'}
+            </button>
+        </div>
+        </div>
     );
 }
 
